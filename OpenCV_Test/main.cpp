215f5446a9a81ea42986548281a67298605d6ff9
niyazidageek <niyazinb@code.edu.az>
2022-01-02 17:06:53 +0400

    Initial Commit

---
+++ OpenCV_Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCV_Test
+//
+//  Created by Niyazi Babayev on 02.01.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
